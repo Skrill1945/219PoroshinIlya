7435bbd8c4db097eacc71e247d4ab3af07b349d6
Skrill1945 <kasyabasya@mail.ru>
2021-09-13 15:53:18 +0300

    Add CW_2

---
+++ CW_2/Task01/Task01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task01
+{
+    class Program
+    {
+        static double b = (1 - Math.Sqrt(5)) / 2;
+
+        static double CalcBine(uint n)
+        {
+            return (int)(((Math.Pow(b, n) - Math.Pow(-b, -n)) / (2 * b - 1)) + 0.5);
+        }
+
+        static void Main(string[] args)
+        {
+            uint n;
+            do
+            {
+                string input = Console.ReadLine();
+                try
+                {
+                    n = uint.Parse(input);
+                    return;
+                } catch { }
+            } while (true);
+            Console.WriteLine(CalcBine(n));
+        }
+    }
+}
---
+++ CW_2/Task01/Task02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task02
+{
+    class Program
+    {
+
+        static double CalcCircleArea(double r)
+        {
+            return Math.PI * r * r;
+        }
+
+        static double CalcCirclePerimeter(double r)
+        {
+            return 2 * Math.PI * r;
+        }
+
+        static void Main(string[] args)
+        {
+            double r;
+            string inp;
+
+            do
+            {
+                Console.Write("Radius: ");
+                inp = Console.ReadLine();
+            } while (!double.TryParse(inp, out r));
+
+            Console.WriteLine($"Area = {CalcCircleArea(r):f4}\nPerimeter = {CalcCirclePerimeter(r):f4}");
+        }
+    }
+}
